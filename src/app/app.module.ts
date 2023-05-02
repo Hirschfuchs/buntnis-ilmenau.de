e728ad6a6504b49e06a3a471fd32315b311ed8ab
Bono Fox <microfuchs@outlook.com>
2023-05-03 00:53:24 +0200

    ➕ #29: Fontawesome hinzugefügt
    - Fontawesome nach Angular-Routine hinzugefügt
    - benötigt für u.a. Github-Logo
    - FA6
    - Free Solid Icons, Free Regular Icons, Free Brands Icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedComponentsModule} from "./shared/components/shared-components.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         BrowserModule,
         AppRoutingModule,
         SharedComponentsModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        FontAwesomeModule
     ],
   providers: [],
   bootstrap: [AppComponent]
