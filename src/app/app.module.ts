2625b3de19943f0270d3bbd8f7fa2fe48b471aec
Bono Fox <microfuchs@outlook.com>
2023-05-03 00:40:24 +0200

    ➕ #29: Material hinzugefügt
    - Material nach Angular-Routine hinzugefügt
    - Custom Theme
    - Globale Styles
    - Animationen einbauen und aktivieren

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedComponentsModule} from "./shared/components/shared-components.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {SharedComponentsModule} from "./shared/components/shared-components.modu
     imports: [
         BrowserModule,
         AppRoutingModule,
-        SharedComponentsModule
+        SharedComponentsModule,
+        BrowserAnimationsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
