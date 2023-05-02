b8d5679bff9c494092065b4509e5faf7ad6396cc
Bono Fox <microfuchs@outlook.com>
2023-05-03 00:27:09 +0200

    🏗️ #29: Modul für geteilte Komponenten
    - Komponenten, die an mehreren Orten benötigt werden in Modul zusammengetragen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SharedComponentsModule} from "./shared/components/shared-components.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        SharedComponentsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
