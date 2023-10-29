96be2a631c1353fcb5aefe1e48bfc23ca8391e30
Bono Fox <microfuchs@outlook.com>
2023-10-29 16:29:30 +0100

    🎨 #52: Prettier angewendet

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -2,17 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 
-
-
 @NgModule({
-    declarations: [
-        HeaderComponent
-    ],
-    exports: [
-        HeaderComponent
-    ],
-    imports: [
-        CommonModule
-    ]
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
+  imports: [CommonModule],
 })
-export class SharedComponentsModule { }
+export class SharedComponentsModule {}
