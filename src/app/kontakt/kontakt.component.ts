d6a3fc7cba29db8003f1bdfe5b4b1a8e207400ae
Bono Fox <microfuchs@outlook.com>
2023-12-09 19:24:07 +0100

    🎉 #66: Hauptseiten hinzugefügt
    - Komponenten hinzugefügt und dynamisch verknüpft:
      - Buntes Ilmenau
      - Kontakt
      - Über uns

---
+++ src/app/kontakt/kontakt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kontakt',
+  standalone: true,
+  imports: [],
+  templateUrl: './kontakt.component.html',
+  styleUrl: './kontakt.component.scss'
+})
+export class KontaktComponent {
+
+}
---
+++ src/app/one-page-areas/ueber-uns/ueber-uns.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UeberUnsComponent } from './ueber-uns.component';
+
+describe('UeberUnsComponent', () => {
+  let component: UeberUnsComponent;
+  let fixture: ComponentFixture<UeberUnsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UeberUnsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(UeberUnsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
