08afdd6c89ab07b422e4338116110971c85ac8bf
Bono Fox <microfuchs@outlook.com>
2023-12-09 18:15:42 +0100

    ✨ #66: Navigation in Header-Bar hinzugefügt

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { BuntnisPageComponent, Mainpage } from '../../types/pages';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  mainpages: Mainpage[] = [
+    {
+      internalName: 'index',
+      url: '',
+      name: 'Buntnis Ilmenau',
+      shortname: 'Buntnis',
+      component: new BuntnisPageComponent(),
+    },
+    {
+      internalName: 'Über uns',
+      url: 'ueber-uns',
+      name: 'Wer sind wir?',
+      shortname: 'Über uns',
+      component: new BuntnisPageComponent(),
+    },
+    {
+      internalName: 'Buntes Ilmenau',
+      url: 'ilmenau-bunt',
+      name: 'Buntnis Ilmenau',
+      shortname: 'Buntnis',
+      component: new BuntnisPageComponent(),
+    },
+    {
+      internalName: 'Kontakt',
+      url: 'kontakt',
+      name: 'Kontakt',
+      component: new BuntnisPageComponent(),
+    },
+  ];
+}
