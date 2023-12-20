b7568fdc4a9a5215052616d0aabff3139ee31bd4
Bono Fox <microfuchs@outlook.com>
2023-12-20 17:30:06 +0100

    🔥 #87: Seite weg

--- src/app/shared/types/pages.ts
+++ src/app/shared/types/pages.ts
@@ -1,22 +1,16 @@
 import { KontaktComponent } from '../../kontakt/kontakt.component';
 import { BuntesIlmenauComponent } from '../../one-page-areas/buntes-ilmenau/buntes-ilmenau.component';
-import { IndexComponent } from '../../one-page-areas/index/index.component';
 import { UeberUnsComponent } from '../../one-page-areas/ueber-uns/ueber-uns.component';
 import { Mainpage } from './pages.model';
 
 export const pages: Mainpage[] = [
   {
-    internalName: 'index',
-    url: '',
-    name: 'Buntnis',
-    pageComponentFrame: { component: IndexComponent },
-  },
-  {
     internalName: 'Über uns',
-    url: 'ueber-uns',
+    url: '',
     name: 'Wer sind wir?',
     shortname: 'Über uns',
     pageComponentFrame: { component: UeberUnsComponent },
+    altNames: ['ueber-uns'],
   },
   {
     internalName: 'Buntes Ilmenau',
