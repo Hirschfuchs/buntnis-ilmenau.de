36c24c75bf8236bbdad9167204a5e7482f7eed28
Bono Fox <microfuchs@outlook.com>
2024-01-14 12:35:20 +0100

    🚚 #85: Generierung der Doku in dist

--- docs/generate-docs.js
+++ docs/generate-docs.js
@@ -1,20 +1,44 @@
 const asciidoctor = require("asciidoctor");
 const { Glob } = require("glob");
-const { copyFile } = require("fs");
+const { copyFile, mkdirSync } = require("fs");
 
 const Asciidoctor = asciidoctor();
 
 console.log("Generiere Dokumentation ...", "...", "...");
 
 const documents = new Glob("docs/**/*.adoc", { nocase: true }).walkSync();
+const docsDir = "dist/buntnis-ilmenau-de/docs";
 
 console.log(`${documents.length} Dateien zu erzeugen`);
 
 for (const document of documents) {
-  console.log(`Erzeuge ${document}`);
+  const fileInfo = extractFileParts(document);
+  console.log(`Erzeuge ${fileInfo.filename} unter ${fileInfo.dir}`);
   // Asciidoctor.convertFile(document);
   Asciidoctor.convertFile(document, {
     safe: "safe",
     attributes: { linkcss: true },
+    mkdirs: true,
+    to_dir: `${docsDir}/${fileInfo.dir}`,
   });
+}
+
+function extractFileParts(filepath) {
+  filepath = filepath.replaceAll("\\", "/");
+
+  if (!filepath.includes("/")) {
+    return { filename: filepath, dir: "" };
+  }
+
+  if (filepath.endsWith("/")) {
+    filepath = filepath.substring(0, filepath.length - 1);
+  }
+
+  return {
+    filename: filepath.substring(
+      filepath.lastIndexOf("/") + 1,
+      filepath.length,
+    ),
+    dir: filepath.substring(0, filepath.lastIndexOf("/")),
+  };
 }
